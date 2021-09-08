5274dc22c1ae1b43648e1b1776608ca7a751bdcc
MPerson92 <76800220+MPerson92@users.noreply.github.com>
2021-09-08 13:32:47 -0400

    ROV2

--- week4/server/server.js
+++ week4/server/server.js
@@ -2,6 +2,8 @@ const express = require("express")
 const app = express()
 const morgan = require('morgan')
 const mongoose = require('mongoose')
+require('dotenv').config()
+const expressJwt = require('express-jwt')
 
 app.use(express.json())
 app.use(morgan('dev'))
@@ -16,13 +18,19 @@ mongoose.connect('mongodb://localhost:27017/rockTheVotedb',
     () => console.log("Connected to the DB")
 )
 
-// app.use("/auth", require("./routes/authRouter"))
+
+app.use("/auth", require("./routes/authRouter"))
+app.use('/api', expressJwt({ secret: process.env.SECRET}))
+
 app.use("/comment", require("./routes/commentRouter"))
 app.use("/issue", require("./routes/issueRouter"))
 app.use("/user", require("./routes/userRouter"))
 
 app.use((err, req, res, next) => {
     console.log(err)
+    if(err.name === "Unauthorized Error"){
+        res.status(err.status)
+    }
     return res.send({errMsg: err.message})
 })
 
